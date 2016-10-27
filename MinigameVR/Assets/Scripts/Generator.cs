331a04c565e19d853c270f5f4cd8b06d3fd817f1
Lars Knaack <lars.knaack@transdocu.de>
2016-10-27 13:56:00 +0200

    Committed Roll-a-Ball VR

---
+++ MinigameVR/Assets/Scripts/Generator.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generator : MonoBehaviour {
+    // Use this for initialization
+    public GameObject hamburgerPrefab;
+    private System.Random rand;
+    private float min;
+    private float max;
+    void Start()
+    {
+        rand = new System.Random();
+        min = -3.0f;
+        max = min * -1;
+
+        GenerateHamburgers();
+        /*
+        foreach (Transform child in transform)
+        {
+            min = -4.0f;
+            max = 4.0f;
+            
+            Vector3 tmp = child.gameObject.transform.position;
+            tmp.x = GetRandomX();
+            child.gameObject.transform.position = tmp;
+        }*/
+    }
+
+    private void GenerateHamburgers()
+    {
+        for (int i = -44; i < 44; i = i + 2)
+        {
+            GameObject tmp = (GameObject)Instantiate(hamburgerPrefab);
+            Vector3 position = new Vector3(GetRandomX(), 0.75f, (float)i);
+            tmp.gameObject.transform.position = position;
+            tmp.gameObject.SetActive(true);
+        }
+    }
+
+    private float GetRandomX()
+    {
+        return (float)rand.NextDouble() * (max - min) + min;
+    }
+}
---
+++ MinigameVR/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.VR;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+
+    private int count;
+    private int total;
+    public Text countText;
+    public Text winText;
+    public AudioSource audio;
+
+    void Start()
+    {
+        total = GameObject.FindGameObjectsWithTag("Collectible").Length;
+        winText.gameObject.SetActive(false);
+        count = 0;
+        SetCountText();
+        VRSettings.renderScale = 1.0f;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Collectible"))
+        {
+            other.gameObject.SetActive(false);
+            audio.Play();
+            count++;
+            SetCountText();
+            if(count >= total)
+            {
+                winText.gameObject.SetActive(true);
+            }
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString() + "/" + total.ToString();
+    }
+}
